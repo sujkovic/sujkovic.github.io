6c774af08db56435b970eed15bf9c598c9bde30c
sujkovic <adrian.sujkovic@gmail.com>
2022-08-19 14:34:34 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='title'>Adrian Sujković</div>
+      <div className='comingSoon'>Website coming soon...</div>
+    </div>
+  );
+}
+
+export default App;
